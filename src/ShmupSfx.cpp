f259126724188b4b284b9fe1a93c29ab90d30e8f
Robin Kirkman <robin.kirkman@gmail.com>
2018-06-04 03:39:05 -0400

    sound in progmem

--- src/ShmupSfx.cpp
+++ src/ShmupSfx.cpp
@@ -14,20 +14,20 @@
 uint8_t ShmupSfx::state1_ = NONE;
 uint8_t ShmupSfx::state2_ = NONE;
 
-const uint16_t off_[] = { 0, 0 };
+const uint16_t bullet_[] PROGMEM = { 250, 3, 0, 0};
+const uint16_t wave_[] PROGMEM = { 7500, 3, 2500, 1, 7500, 3, 2500, 1, 7500, 3, 2500, 1, 0, 0 };
+const uint16_t beam_[] PROGMEM = { 2000, 1, 0, 0 };
+const uint16_t enemy_impact_[] PROGMEM = { 1000, 2, 900, 2, 800, 2, 700, 2, 0, 0 };
+const uint16_t player_impact_[] PROGMEM = { 1000, 3, 1200, 3, 1400, 3, 1600, 3, 0, 0 };
 
-const uint16_t bullet_[] = { 250, 3, 0, 0};
-const uint16_t wave_[] = { 7500, 3, 2500, 1, 7500, 3, 2500, 1, 7500, 3, 2500, 1, 0, 0 };
-const uint16_t beam_[] = { 2000, 1, 0, 0 };
-const uint16_t enemy_impact_[] = { 1000, 2, 900, 2, 800, 2, 700, 2, 0, 0 };
-const uint16_t player_impact_[] = { 1000, 3, 1200, 3, 1400, 3, 1600, 3, 0, 0 };
-
-const uint16_t *sfx1_ = off_;
-const uint16_t *sfx2_ = off_;
+const uint16_t *sfx1_;
+const uint16_t *sfx2_;
 
 void ShmupSfx::sfx(uint8_t priority, const uint16_t *sptr) {
-	uint16_t freq = *(sptr++);
-	uint16_t duration = *(sptr++);
+	uint16_t freq = pgm_read_word(sptr++);
+	if (freq == 0)
+		return;
+	uint16_t duration = pgm_read_word(sptr++);
 	if (priority > state1_) {
 		state1_ = priority;
 		sfx1_ = sptr;
@@ -47,8 +47,6 @@ void ShmupSfx::begin() {
 void ShmupSfx::beginGame() {
 	state1_ = NONE;
 	state2_ = NONE;
-	sfx1_ = off_;
-	sfx2_ = off_;
 	BeepPin1::noTone();
 	BeepPin2::noTone();
 }
@@ -64,22 +62,14 @@ void ShmupSfx::tick() {
 	BeepPin1::timer();
 	BeepPin2::timer();
 	if (!BeepPin1::duration && state1_) {
-		uint16_t freq = *(sfx1_++);
-		uint16_t duration = *(sfx1_++);
-		if (freq == 0) {
-			state1_ = NONE;
-		} else {
-			BeepPin1::tone(BeepPin1::freq(freq), duration);
-		}
+		uint8_t s = state1_;
+		state1_ = NONE;
+		sfx(s, sfx1_);
 	}
 	if (!BeepPin2::duration && state2_) {
-		uint16_t freq = *(sfx2_++);
-		uint16_t duration = *(sfx2_++);
-		if (freq == 0) {
-			state2_ = NONE;
-		} else {
-			BeepPin2::tone(BeepPin2::freq(freq), duration);
-		}
+		uint8_t s = state2_;
+		state2_ = NONE;
+		sfx(s, sfx2_);
 	}
 }
 
